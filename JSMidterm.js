5f385547e793bf610c6cde6c5ef88873fee8e75a
Sophie Parsonnet <sopa3708@colorado.edu>
2023-06-02 15:56:52 -0600

    js midterm

--- JSMidterm.js
+++ JSMidterm.js
@@ -3,6 +3,13 @@
 document.getElementById("demo").innerHTML = Math.floor(Math.random() * 101);
 
 function Random() {
-    var rnd = Math.floor(Math.random() * 101);
+    var rnd = Math.floor(Math.random() * 50);
     document.getElementById('random').value = rnd;
+}
+
+document.getElementById("demo").innerHTML = Math.floor(Math.random() * 101);
+
+function Random2() {
+    var rnd = Math.floor(Math.random() * 50)+51;
+    document.getElementById('number2').value = rnd;
 }
